65adaf528ff2def0fe2d8f4a435c3989a8c31ab2
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-04-10 06:22:59 +0600

    add the json data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,9 +9,11 @@ const Home = () => {
     return (
         <div>
             <div className=' bg-gradient-to-r from-purple-50 to-purple-100 pt-12'>
-                <div className='my-container md:flex gap-8'>
+                <div className='my-container gap-8 flex xs:flex-col flex-col-reverse md:flex-row'>
                     <div className='md:w-1/2 text-center'>
-                        <h1 className='md:text-7xl text-5xl font-bold md:pt-36 '>One Step<br/>Closer To Your <br /><span className='text-purple-500'>Dream Job</span></h1>
+                        <h1 className='md:text-7xl text-5xl font-bold md:pt-16'>One Step<br/>Closer To Your <br /><span className='text-indigo-500'>Dream Job</span></h1>
+                        <p className='text-xl text-gray-500 my-8'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                        <button className='btn2'>Get Started</button>
                     </div>
                     <div className='md:w-1/2'>
                         <img  src={pic} alt="" />
@@ -20,7 +22,7 @@ const Home = () => {
             </div>
             <div className='my-container text-center mt-24'>
                 <h2 className='text-5xl font-bold my-5'>Job Category List</h2>
-                <p className='text-lg'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+                <p className='text-xl text-gray-500'>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
             <div className='my-container md:grid grid-cols-4 gap-4 mt-12'>
                 {
--- src/components/JobCategory.jsx
+++ src/components/JobCategory.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const JobCategory = ({jobCategory}) => {
     const{img,title,numberOfPost}=jobCategory;
     return (
-        <div className='bg-gradient-to-r from-purple-50 to-pink-50 rounded-md p-8'>
+        <div className='bg-gradient-to-r from-purple-50 to-pink-50 rounded-md p-8 mt-4'>
             <img className='h-16 w-16 text-blue-500' src={img} alt="" />
-            <h3 className='text-xl font-bold'>{title}</h3>
-            <p><small>{numberOfPost}</small></p>
+            <h3 className='text-xl font-bold mt-4'>{title}</h3>
+            <p>{numberOfPost}</p>
         </div>
     );
 };
